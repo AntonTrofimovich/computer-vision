3a4f3e9ec0a7bf8184f2aaa0c5a4c1b1a1c9e094
Anton Trafimovich <antont@compatibl.com>
2020-06-03 09:44:39 +0300

    Changed stroke width in lab1; Added lab2;

--- lab1.py
+++ lab1.py
@@ -3,13 +3,15 @@ from graphics import GraphWin, Line, Circle, Oval, Point
 win = GraphWin(width=620, height=250)
 
 line = Line(Point(10, 10), Point(190, 190))
+line.setWidth(2)
 line.draw(win)
 
 circle = Circle(Point(350, 110), 100)
+circle.setWidth(2)
 circle.draw(win)
 
 ellipse = Oval(Point(510, 10), Point(610, 190))
-ellipse.setFill('green');
+ellipse.setWidth(2)
 ellipse.draw(win)
 
 win.getMouse()
